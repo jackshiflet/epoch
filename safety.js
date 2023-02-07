0fd0173b2b14eb5ac6e9d84de8a7ea685a1b30b1
jackshiflet <90656362+jackshiflet@users.noreply.github.com>
2023-02-06 21:06:31 -0700

    Add files via upload

---
+++ safety.js
@@ -0,0 +1,71 @@
+let sideMenuOpen = false;
+
+function openSideBar(){
+    if (!sideMenuOpen){
+        document.getElementById("sidebar").style.width = "20vw";
+        document.getElementById("navigation").style.visibility = "hidden"
+        document.getElementById("filter-object").style.width = "100%";
+        document.getElementById("filter-object").style.height = "100%";
+        document.getElementById("side-menu-exit-icon").style.visibility = "visible"
+        document.getElementById("side-menu-list").style.visibility = "visible"
+        document.getElementById("side-menu-list").style.transitionDelay = ".4s"
+        document.getElementById("side-menu-exit-icon").style.transitionDelay = ".4s"
+        document.getElementById("navigation").style.filter = "brightness(50%)"
+        document.getElementById("background-img-1").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-1").style.filter = "brightness(50%)"
+        document.getElementById("background-img-2").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-2").style.filter = "brightness(50%)"
+        document.getElementById("background-img-3").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-3").style.filter = "brightness(50%)"
+        document.getElementById("background-img-4").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-4").style.filter = "brightness(50%)"
+        document.getElementById("background-img-5").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-5").style.filter = "brightness(50%)"
+        document.getElementById("background-img-6").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-6").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-7").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-8").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-9").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-10").style.filter = "brightness(50%)"
+        document.getElementById("background-img-text-11").style.filter = "brightness(50%)"
+
+
+    }
+    sideMenuOpen = true;
+}
+
+function closeSideBar(){
+    if (sideMenuOpen){
+
+        document.getElementById("sidebar").style.width = "0";
+        document.getElementById("filter-object").style.width = "0";
+        document.getElementById("filter-object").style.height = "0";
+        document.getElementById("navigation").style.visibility = "visible";
+        document.getElementById("side-menu-exit-icon").style.visibility = "hidden";
+        document.getElementById("side-menu-list").style.visibility = "hidden"
+        document.getElementById("side-menu-exit-icon").style.transitionDelay = "0s"
+        document.getElementById("side-menu-list").style.transitionDelay = "0s"
+        document.getElementById("navigation").style.filter = "none"
+        document.getElementById("background-img-1").style.filter = "none"
+        document.getElementById("background-img-text-1").style.filter = "none"
+        document.getElementById("background-img-2").style.filter = "none"
+        document.getElementById("background-img-text-2").style.filter = "none"
+        document.getElementById("background-img-3").style.filter = "none"
+        document.getElementById("background-img-text-3").style.filter = "none"
+        document.getElementById("background-img-4").style.filter = "none"
+        document.getElementById("background-img-text-4").style.filter = "none"
+        document.getElementById("background-img-5").style.filter = "none"
+        document.getElementById("background-img-text-5").style.filter = "none"
+        document.getElementById("background-img-6").style.filter = "none"
+        document.getElementById("background-img-text-6").style.filter = "none"
+        document.getElementById("background-img-text-7").style.filter = "none"
+        document.getElementById("background-img-text-8").style.filter = "none"
+        document.getElementById("background-img-text-9").style.filter = "none"
+        document.getElementById("background-img-text-10").style.filter = "none"
+        document.getElementById("background-img-text-11").style.filter = "none"
+
+
+        document.body.style.overflowY = "visible"
+    }
+    sideMenuOpen = false;
+}
